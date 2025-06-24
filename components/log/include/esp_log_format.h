f318f33744708b6004934fdeafebfde9f8563122
0xFEEDC0DE64 <daniel@brunner.ninja>
2025-06-24 17:37:03 +0200

    feat(log): Add filename and line number back to esp log

--- components/log/include/esp_log_format.h
+++ components/log/include/esp_log_format.h
@@ -11,8 +11,12 @@ extern "C" {
 #endif
 
 /** @cond */
+
+#define ESP_LOG_STRINGIFY2(x) #x
+#define ESP_LOG_STRINGIFY(x) ESP_LOG_STRINGIFY2(x)
+
 // For backward compatibility (these macros are not used in the log v2).
-#define LOG_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%" PRIu32 ") %s: " format LOG_RESET_COLOR "\n"
+#define LOG_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " %s:" ESP_LOG_STRINGIFY(__LINE__) " (%" PRIu32 ") %s: " format LOG_RESET_COLOR "\n", (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
 #define _ESP_LOG_DRAM_LOG_FORMAT(letter, format)  DRAM_STR(#letter " %s: " format "\n")
 #define LOG_SYSTEM_TIME_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%s) %s: " format LOG_RESET_COLOR "\n"
 /** @endcond */
