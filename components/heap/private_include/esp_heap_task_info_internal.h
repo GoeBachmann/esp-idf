b3b42c97184887b845a68e8e016a3c70a600df9f
Ferdinand Bachmann <ferdinand.bachmann@go-e.com>
2026-06-19 12:36:14 +0200

    feat(heap): alloc tracking per arbitrary user-defined subtask
    This allows users to track allocations from arbitrary scopes of their app.

--- components/heap/private_include/esp_heap_task_info_internal.h
+++ components/heap/private_include/esp_heap_task_info_internal.h
@@ -16,6 +16,9 @@ typedef struct heap_t_ heap_t;
 
 typedef struct {
     void * task_info;
+#ifdef CONFIG_HEAP_TASK_TRACKING_PER_USER_SUBTASK
+    const char * subtask;
+#endif // CONFIG_HEAP_TASK_TRACKING_PER_USER_SUBTASK
 } heap_caps_block_owner_t;
 
 void heap_caps_update_per_task_info_alloc(heap_t *heap, void *ptr, size_t size, uint32_t caps);
